bebed73fd37d7893c3d8801de1af3a69bb72a7fd
mengzhennan <1026251951@qq.com>
2018-04-26 15:08:28 +0800

    List user replies

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -50,4 +50,9 @@ class Topic extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
