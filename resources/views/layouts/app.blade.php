6cadc203e1160a561ee59096b9cb1f2aba51b030
mengzhennan <1026251951@qq.com>
2018-04-28 16:37:47 +0800

    Admin permissions

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <title>@yield('title', 'LaraBBS') - {{ setting('site_name', 'Laravel 进阶教程') }}</title>
     <meta name="description" content="@yield('description', setting('seo_description', 'LaraBBS'))" />
     <meta name="keyword" content="@yield('keyword', setting('seo_keyword', 'LaraBBS'))" />
-    
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @yield('styles')
